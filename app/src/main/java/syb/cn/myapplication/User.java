3480ce1dee15ecab3bd8135cebc0b511f1921921
shenyb <shen983@sohu.com>
2015-02-12 22:48:44 +0800

    Merge branch 'master' of https://github.com/shenyb/AndroidDemo
    Conflicts:
    	app/src/main/java/syb/cn/myapplication/User.java

--- app/src/main/java/syb/cn/myapplication/User.java
+++ app/src/main/java/syb/cn/myapplication/User.java
@@ -7,7 +7,7 @@ public class User {
     private String name;
     private int age;
     private  int id;
-    private int sex4;
+    private int sex;
 
     public String getName() {
         return name;
